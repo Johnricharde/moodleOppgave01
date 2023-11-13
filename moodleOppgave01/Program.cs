d74242e4d4d6339a71ed6025ba017791a4e4892f
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-13 13:21:02 +0100

    Add project files.

---
+++ moodleOppgave01/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace moodleOppgave01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei, hva heter du?:");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hallaien {userName}!");
+            Console.ReadKey();
+        }
+    }
+}
